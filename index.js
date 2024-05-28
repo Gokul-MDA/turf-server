ee94964c3d599993342332d0e27b956121e3a545
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2024-05-28 10:17:43 +0530

    slots api updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { conneciton } from './utils/helpers.js'
 import slotRoutes from "./router/slot.routes.js"
 
 const app = express()
+app.use(express.json())
 
 app.use("/api", slotRoutes)
 
--- router/slot.routes.js
+++ router/slot.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getSlots } from "../service/slotService.js";
+import { bookSlot, getSlots } from "../service/slotService.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/slots",getSlots)
+router.get("/slots", getSlots);
+router.post("/slots", bookSlot);
 
-export default router
+export default router;
